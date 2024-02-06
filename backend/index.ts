b322aa85fb446a66951b19f90c2aeb2c128acdc6
Hrithik Raj <hrithik.raj@tridiagonal.com>
2024-02-07 04:22:27 +0530

    refactoring: origin removed

--- backend/index.ts
+++ backend/index.ts
@@ -1,19 +1,19 @@
 import express from "express";
-import { createServer } from "http";
+import http from "http"; // Change this line
 import { Server } from "socket.io";
 import cors from "cors";
 import { socketController } from "./controller/socketController";
 
 const app = express();
-app.use(cors({ origin: "http://localhost:3000" }));
-const httpServer = createServer(app);
+app.use(cors({ origin: "*" }));
+const httpServer = http.createServer(app); // Change this line
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:3000", // The allowed origin
+    origin: "*", // The allowed origin
     methods: ["GET", "POST", "PUT"], // Allowed HTTP methods
     credentials: true, // If credentials are needed
   },
 });
-io.on("connection",socketController);
+io.on("connection", socketController);
 
-httpServer.listen(5000);
+httpServer.listen("5000");
